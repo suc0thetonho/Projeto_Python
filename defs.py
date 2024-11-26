8654ea54ded8f5dd2bfabf092f49f7b6b3f23dc5
Antonio Carlos <antonio_verissimo1@outlook.com>
2024-11-25 21:31:20 -0300

    adicionando o arquivo defs, onde irá ficar as funções, atualmente so possui cadastro e ler o arquivo cadastro

---
+++ defs.py
@@ -0,0 +1,21 @@
+
+
+
+cadastro = []
+
+
+
+def salvar_cadastro(login, senha):
+    with open("user_login.txt", "a") as arquivo:
+        arquivo.write(f"{login}:{senha}\n")
+
+def load_cadastros():
+    try:
+        with open('user_logins.txt', 'r') as arquivo:
+            for linha in arquivo:
+                linha = linha.strip().split(":", 1)
+                if len(linha) == 2:
+                    login, senha = linha
+                    cadastro.append({"login": login.strip(), "senha": senha.strip()})
+    except FileNotFoundError:
+        pass
